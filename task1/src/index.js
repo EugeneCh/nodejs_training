916db19f7648350b79b84a857412f35bfa430ac3
Eugene <Yauhen_Chaika@epam.com>
2017-09-07 20:29:01 +0300

    Added eslint support and homework

--- task1/src/index.js
+++ task1/src/index.js
@@ -1,1 +1,9 @@
-console.log("LALAL");
+import config from '../config/config.json';
+
+import User from './models/user';
+import Product from './models/product';
+
+console.log(`Name ${config.name} from config`);
+
+new User();
+new Product();
