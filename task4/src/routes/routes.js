152abda5e86a59924acb6b375cc4efecb7b347e2
Yauhen Chaika <ychaika@slb.com>
2017-11-01 10:33:31 +0300

    Add some routes

--- task4/src/routes/routes.js
+++ task4/src/routes/routes.js
@@ -6,4 +6,24 @@ routes.get('/', (req, res) => {
     res.json({ok: true});
 });
 
+routes.get('/api/products', (req, res) => {
+    res.json({product: 'all products'});
+});
+
+routes.get('/api/products/:id', (req, res) => {
+    res.json({product: 'specific product'});
+});
+
+routes.get('/api/products/:id/reviews', (req, res) => {
+    res.json({review: 'reviews for product'});
+});
+
+routes.post('/api/products', (req, res) => {
+    res.json({ok: true});
+});
+
+routes.get('/api/users', (req, res) => {
+    res.json({users: 'all users'});
+});
+
 export default routes;
