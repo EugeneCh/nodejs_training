1f15fdf4e6cd5ca168a74871bc142b87067e4636
unknown <eugenik10@gmail.com>
2017-11-18 13:26:36 +0300

    Added db models to routes

--- task6/src/routes/routes.js
+++ task6/src/routes/routes.js
@@ -2,11 +2,13 @@ import {Router} from 'express';
 import jwt from 'jsonwebtoken';
 import passport from 'passport';
 
-import {Products} from '../controllers/products.controller';
-import {Users} from '../controllers/users.controller';
 import {getErrorResponse, getTokenResponse} from '../utils/utils';
 import {checkToken} from '../middlewares/check-token';
 import {PRIVATE_KEY} from '../utils/constants';
+
+const User = require('./../models').User;
+const Product = require('./../models').Product;
+const Review = require('./../models').Review;
 
 const routes = Router();
 
@@ -15,53 +17,61 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/api/products', checkToken, (req, res) => {
-    const products = Products.all();
-    res.json(products);
+    Product.all()
+        .then(products => res.send(JSON.stringify(products)))
+        .error(error => res.send(error));
 });
 
 routes.get('/api/products/:id', checkToken, (req, res) => {
-    const product = Products.getProductById(+req.params.id);
-    res.json(product);
+    Product.findById(req.params.id)
+        .then(product => res.send(JSON.stringify(product)))
+        .error(error => res.send(error));
 });
 
-routes.get('/api/products/:id/reviews', checkToken, (req, res) => {
-    const review = Products.getReviewsById(+req.params.id);
-    res.json(review);
-});
+// routes.get('/api/products/:id/reviews', checkToken, (req, res) => {
+//     const review = Products.getReviewsById(+req.params.id);
+//     res.json(review);
+// });
 
 routes.post('/api/products', checkToken, (req, res) => {
-    let newProduct = Products.addNewProduct();
-    res.json(newProduct);
+    Product.create({
+        name: 'New Product',
+        value: '1',
+        review: ['review']
+    })
+        .then(product => res.send(JSON.stringify(product)))
+        .error(error => res.send(error));
 });
 
 routes.get('/api/users', checkToken, (req, res) => {
-    const users = Users.all();
-    res.json(users);
+    User.all()
+        .then(users => res.send(JSON.stringify(users)))
+        .error(error => res.send(error));
 });
 
 routes.post('/auth', (req, res) => {
     const {username, password} = req.body;
-    const users = Users.all();
-    const user = users.find(user => user.username === username);
+    User.findOne({ where: {username: username} })
+        .then(user => {
+            res.setHeader('Content-Type', 'application/json');
 
-    res.setHeader('Content-Type', 'application/json');
-
-    if (user && user.password === password) {
-        jwt.sign({username: user.username}, PRIVATE_KEY, (err, token) => {
-            res.send(JSON.stringify(getTokenResponse(user.username, user.email, token)));
-        });
-    } else {
-        res.send(JSON.stringify(getErrorResponse()));
-    }
-
+            if (user && user.password === password) {
+                jwt.sign({username: user.username}, PRIVATE_KEY, (err, token) => {
+                    res.send(JSON.stringify(getTokenResponse(user.username, user.email, token)));
+                });
+            } else {
+                res.send(JSON.stringify(getErrorResponse()));
+            }
+        })
+        .error(error => res.send(error));
 });
 
-routes.post('/authenticate', passport.authenticate('local', {session: false}), (req, res) => {
-    const {username, email} = req.user;
-
-    jwt.sign({username: username}, PRIVATE_KEY, (err, token) => {
-        res.send(JSON.stringify(getTokenResponse(username, email, token)));
-    });
-});
+// routes.post('/authenticate', passport.authenticate('local', {session: false}), (req, res) => {
+//     const {username, email} = req.user;
+//
+//     jwt.sign({username: username}, PRIVATE_KEY, (err, token) => {
+//         res.send(JSON.stringify(getTokenResponse(username, email, token)));
+//     });
+// });
 
 export default routes;
